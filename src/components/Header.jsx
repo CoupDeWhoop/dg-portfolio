7270ba77f25fabcb8f783bd046ab34461cc423ad
CoupDeWhoop <greenland.david@gmail.com>
2024-07-03 17:47:39 +0100

    added email icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,38 @@
-import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 
-export default function Header () {
-    return (
-        <header className="header">
-            <div className="header-card">
-                <h1>Dave Greenland</h1>
-                <div className="job-title-container">
-                    <img
-                    className="profile-image"
-                    src="/dev-images/web-developer.svg"
-                    alt="Web Developer"
-                    />
-                    <p>software developer</p>                
-                </div>
-                <div className="social-links">
-                    <a href="https://www.linkedin.com/in/david-greenland-360aba291/" target="_blank" rel="noopener noreferrer">
-                        <FaLinkedin size={38} />
-                    </a>
-                    <a href="https://github.com/CoupDeWhoop" target="_blank" rel="noopener noreferrer">
-                        <FaGithub size={38} />
-                    </a>
-                </div>
-                <div>
-                    <h3><a href="mailto:greenlanddev01@gmail.com">greenlanddev01@gmail.com</a></h3>
-                </div>
-            </div>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header-card">
+        <h1>Dave Greenland</h1>
+        <div className="job-title-container">
+          <img
+            className="profile-image"
+            src="/dev-images/web-developer.svg"
+            alt="Web Developer"
+          />
+          <p>software developer</p>
+        </div>
+        <div className="social-links">
+          <a
+            href="https://www.linkedin.com/in/david-greenland-360aba291/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin size={38} />
+          </a>
+          <a
+            href="https://github.com/CoupDeWhoop"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub size={38} />
+          </a>
+          <a href="mailto:greenlanddev01@gmail.com">
+            <FaEnvelope size={38} />
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 }
