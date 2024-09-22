408c9aced53c513146845f64d2a23e3cbe1ff9e7
Dave Greenland <118599454+CoupDeWhoop@users.noreply.github.com>
2024-09-22 22:33:34 +0100

    remove space!

--- src/components/ScrollSection.jsx
+++ src/components/ScrollSection.jsx
@@ -2,7 +2,7 @@ import Projects from "./Projects"
 export default function ScrollSection() {
     return (
         <section className="scroll-container">
-            <p className="intro-text"><strong>Hello !</strong><br/>
+            <p className="intro-text"><strong>Hello!</strong><br/>
                 <br/>
             I'm a back-end software developer working at Bright HR in Manchester. Having begun as a Javascript Developer, I'm now primarily working with C#. I previously worked as a music teacher in Bolton for 16 years and continue to play the trumpet.</p>
             <Projects />
