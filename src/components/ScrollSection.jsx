e88d51f322a3a0e51cb8a79f45c8b65ae79c0f7d
Dave Greenland <118599454+CoupDeWhoop@users.noreply.github.com>
2024-09-22 22:23:49 +0100

    Update  biog

--- src/components/ScrollSection.jsx
+++ src/components/ScrollSection.jsx
@@ -2,10 +2,8 @@ import Projects from "./Projects"
 export default function ScrollSection() {
     return (
         <section className="scroll-container">
-            <p className="intro-text">Based in Manchester, I am a motivated developer working hard each day to take my learning further. I thrive in a collaborative environment and am adept at working with and motivating others. With 15 years of experience as a teacher and a life-long commitment to mastering the trumpet, I bring not only technical skills but also creativity and personality to every project.<br/>
-            <br/>
-             If not working on solving problems with code or playing the trumpet, I can probably be found making (or eating) bread and pizza.</p>
-            <br/>
+            <p className="intro-text"><em>Hello !</em><br/>
+            I'm a back-end software developer working at Bright HR in Manchester. Having begun as a Javascript Developer, I'm now primarily working with C#. I previously worked as a music teacher in Bolton for 16 years and continue to play the trumpet. 
             <Projects />
         </section>
     )
