34020f7eda4e24ced8007e58b3a3eb000291ecf4
Dave Greenland <118599454+CoupDeWhoop@users.noreply.github.com>
2024-09-22 22:26:57 +0100

    missed closing tag

--- src/components/ScrollSection.jsx
+++ src/components/ScrollSection.jsx
@@ -3,7 +3,7 @@ export default function ScrollSection() {
     return (
         <section className="scroll-container">
             <p className="intro-text"><em>Hello !</em><br/>
-            I'm a back-end software developer working at Bright HR in Manchester. Having begun as a Javascript Developer, I'm now primarily working with C#. I previously worked as a music teacher in Bolton for 16 years and continue to play the trumpet. 
+            I'm a back-end software developer working at Bright HR in Manchester. Having begun as a Javascript Developer, I'm now primarily working with C#. I previously worked as a music teacher in Bolton for 16 years and continue to play the trumpet.</p>
             <Projects />
         </section>
     )
